1d51f16aad18af22bb045111dcbaaeb3a87fbd86
000Underscore <victor.leclaircie@gmail.com>
2025-11-26 17:02:00 +0100

    Refactor crew page structure and styles for improved layout; update crew data for consistency and clarity.

--- js/crew-data.js
+++ js/crew-data.js
@@ -1,21 +1,21 @@
 const crewData = {
   douglas: {
     name: "DOUGLAS HURLEY",
-    role: "COMMANDER",
-    image: "/img/Douglas_Hurley.png",
+    role: "Commander",
+    image: "img/Douglas_Hurley.png",
     description:
       "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
   },
   mark: {
     name: "MARK SHUTTLEWORTH",
     role: "Mission Specialist",
-    image: "/img/Mark_Shuttleworth.png",
+    image: "img/Mark_Shuttleworth.png",
     description:
       "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
   },
   victor: {
     name: "Victor Glover",
-    role: "PILOT",
+    role: "Pilot",
     image: "img/Victor_Glover.png",
     description:
       "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
